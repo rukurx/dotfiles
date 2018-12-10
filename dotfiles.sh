5c51e39dc134a1707e9c83cde28aeeea32f21d26
rukurx <rukurx@users.noreply.github.com>
2018-12-10 18:09:17 +0900

    dotfiles.sh修正

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-ln -sf ~/dotfiles/vim/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/zsh/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/zsh/.zpreztorc ~/.zpreztorc
+ln -sf ~/workspace/rukurx/dotfiles/vim/.vimrc ~/.vimrc
+ln -sf ~/workspace/rukurx/dotfiles/zsh/.zshrc ~/.zshrc
+ln -sf ~/workspace/rukurx/dotfiles/git/.gitconfig ~/.gitconfig
